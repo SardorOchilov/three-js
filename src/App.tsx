852eb77219d132d1aa8be73d88ccc53361166eec
SardorOchilov <ochilovsardor45@gmail.com>
2025-03-29 16:04:26 +0500

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import ElephantModel from './components/ElephantModel';
+import cls from './styles/app.module.scss';
+import JunniIcon from "./assets/icons/junni.tsx";
 
 function App() {
     return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <ElephantModel />
+        <div className={cls.main} style={{width: '100vw', height: '100vh'}}>
+            <header>
+                <div className={cls.icon}>
+                    <JunniIcon/>
+                </div>
+            </header>
+            <ElephantModel/>
         </div>
     );
 }
--- src/components/ElephantModel.tsx
+++ src/components/ElephantModel.tsx
@@ -4,12 +4,15 @@ import {OrbitControls, useGLTF} from '@react-three/drei';
 function Elephant() {
     const {scene} = useGLTF('/assets/elephant.glb'); // GLB modeliga yo'l
 
-    return <primitive position={[0, -12, 0]} object={scene} scale={200}/>;
+    return <group position={[0, -39, 0]} rotation={[0, 0, 0]} >
+        <primitive position={[0, 0, 0]} rotation={[0, -1, 0]} object={scene} scale={600}/>
+    </group>
+    ;
 }
 
 const ElephantModel = () => {
     return (
-        <Canvas camera={{position: [0, 2, 5], fov: 50}} >
+        <Canvas camera={{position: [0, 1, 5], fov: 50}}>
             <ambientLight intensity={0.5}/>
             <directionalLight position={[2, 5, 2]}/>
             <OrbitControls/>
